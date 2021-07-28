cfdb0861f44f01affd4e37a09e3115eac85bbd1b
Matthew Phillips <matthew@skypack.dev>
2021-07-27 20:11:59 -0400

    Make a vanilla counter

--- demo/src/components/Vanilla.js
+++ demo/src/components/Vanilla.js
@@ -8,7 +8,18 @@ class VanillaElement extends HTMLElement {
   }
   connectedCallback() {
     let div = document.createElement('div');
+    let btn = document.createElement('button');
+    let span = document.createElement('span');
+    span.textContent = 'Count: 0';
+    let count = 0;
+    btn.addEventListener('click', () => {
+      count++;
+      span.textContent = `Count: ${count}`;
+    });
+    btn.textContent = 'Increment';
     div.textContent = 'This is a Vanilla element';
+    div.append(btn);
+    div.append(span);
     this.shadowRoot.append(div);
   }
 }
